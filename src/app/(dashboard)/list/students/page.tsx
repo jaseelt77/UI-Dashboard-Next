1deb332f03df4396ed493af0adacc52d58c5b866
jaseel <jaseel.t@mfine.co>
2025-03-19 10:58:40 +0530

    students page new features

--- src/app/(dashboard)/list/students/page.tsx
+++ src/app/(dashboard)/list/students/page.tsx
@@ -46,25 +46,26 @@ const columns = [
 ]; 
 
 const renderRaw = (item: StudentList) => (
-        <tr
+      <tr
         key={item.id}
         className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
       >
        <td className="flex items-center gap-4 p-4">
-        <Image
-          src={item.img}
+       <Image
+          src={item.img || "/noAvatar.png"}
           alt=""
           width={40}
           height={40}
           className="md:hidden xl:block w-10 h-10 rounded-full object-cover"
         />
+
         <div className="flex flex-col">
           <h3 className="font-semibold">{item.name}</h3>
-          <p className="text-xs text-gray-500">{item.class}</p>
+          <p className="text-xs text-gray-500">{item.class.name}</p>
         </div>
       </td>
         <td className="hidden md:table-cell">{item.username}</td>
-      <td className="hidden md:table-cell">{item.grade}</td>
+      <td className="hidden md:table-cell">{item.class.name[0]}</td>
       <td className="hidden md:table-cell">{item.phone}</td>
       <td className="hidden md:table-cell">{item.address}</td>
         <td>
